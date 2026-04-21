3d5fab0c61dc83ec40984b7ff54fd8ba3168ee8b
joshuarabulan <joshuamacatangayrabulan.com>
2026-04-21 09:33:01 +0800

    new

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const { createFeedback, getFeedback, deleteAllFeedback, deleteFeedback } = require('../controllers/feedbackController');
+const feedbackController = require('../controllers/feedbackController');
+
+console.log('Available methods:', Object.keys(feedbackController));
+console.log('createFeedback:', typeof feedbackController.createFeedback);
+console.log('getFeedback:', typeof feedbackController.getFeedback);
+console.log('deleteFeedback:', typeof feedbackController.deleteFeedback);
+console.log('deleteAllFeedback:', typeof feedbackController.deleteAllFeedback);
+
+const { createFeedback, getFeedback, deleteAllFeedback, deleteFeedback } = feedbackController;
 
 const router = express.Router();
 
 router.post('/submit', createFeedback);
-router.post('/get-feedback', getFeedback);
-
+router.get('/get-feedback', getFeedback);
 router.delete('/feedback/:id', deleteFeedback);
 router.delete('/feedback', deleteAllFeedback);
-
 
 module.exports = router;
